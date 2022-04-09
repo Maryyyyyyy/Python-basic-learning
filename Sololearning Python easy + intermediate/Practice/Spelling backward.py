84be8936944554caca354ca1ae68c4f0362adc4f
ccm1997happy@gmail.com <46562598+ChunmoChen@users.noreply.github.com>
2022-04-08 22:50:42 -0400

    Finish part 2 functional

--- Sololearning Python easy + intermediate/Practice/Spelling backward.py
+++ Sololearning Python easy + intermediate/Practice/Spelling backward.py
@@ -1,1 +1,10 @@
 # given a string as input, use recursion to output each letter of the string in reverse order, on a new line
+def spell(name):
+    name = list(name)
+    reverse = name[::-1]
+    for i in reverse:
+        print(i)
+
+txt = str(raw_input("What is the word?"))
+spell(txt)
+

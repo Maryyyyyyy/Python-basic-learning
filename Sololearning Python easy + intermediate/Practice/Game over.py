efc470a8fbd85d28665cff70272ecbd614f5a6aa
ccm1997happy@gmail.com <46562598+ChunmoChen@users.noreply.github.com>
2022-04-29 18:39:40 -0400

    add assertion argument in game over file

--- Sololearning Python easy + intermediate/Practice/Game over.py
+++ Sololearning Python easy + intermediate/Practice/Game over.py
@@ -2,7 +2,12 @@
 # Complete the code to take the name and level from user input, create a player object with the corresponding values and call the intro() method of that object
 
 class Player:
-    def __init__(self,name,level):
+    def __init__(self,name:str,level):
+        #Validation to receive argument
+        # if values is used here, we can use assert argument to rule out value that is not in expectation
+        # assert level >=0, f"level {level} is not greater or equal to zero" 
+        # if the input level is smaller than 0, it will run as assertionerror "level is not greater or equal to zero"
+        #Assign to self object
         self.name = name
         self.level = level
     def intro(self):
